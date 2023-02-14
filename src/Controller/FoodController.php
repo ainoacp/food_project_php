ea690a53e18975f733b71c93cff38f4361b81da3
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-14 16:28:17 +0000

    first commit

---
+++ src/Controller/FoodController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Food;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FoodController extends AbstractController
+{
+    #[Route('/food', name: 'app_food')]
+    public function index(EntityManagerInterface $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Food::class);
+        $arrayRecepies = $repository->findAll();
+        return $this->render('food/showRecipes.html.twig', ["food" => $arrayRecepies]);
+    }
+
+    #[Route('/food/new', name: 'add_food')]
+    public function addFood(EntityManagerInterface $doctrine): Response
+    {
+        $food1 = new Food();
+        $food1->setName('Roquefort croquettes');
+        $food1->setImage('https://www.aromamanchego.com/recursos/imagenes/productos/Imagenes_fichas/Croquetas_queso_azul.jpg');
+        $food1->setIngredients(['Flour', 'Milk', 'Roquefort cheese', 'AOVE', 'Salt', 'Black pepper', 'Nutmeg']);
+        $food1->setType('Starter');
+        $food1->setOrigin('France');
+        $food1->setTime('30');
+        $food1->setDifficulty('Medium');
+
+        $food2 = new Food();
+        $food2->setName('Cheese cake');
+        $food2->setImage('https://www.recetasjudias.com/wp-content/uploads/2019/12/Cheesecake2.jpg');
+        $food2->setIngredients(['Cream', 'Soft cheese', 'Sugar', 'Gelatine', 'Cookies', 'Butter', 'Strawberry Jam']);
+        $food2->setType('Dessert');
+        $food2->setOrigin('EEUU');
+        $food2->setTime('35');
+        
+        $food3 = new Food();
+        $food3->setName('Lasagna');
+        $food3->setImage('https://www.recetasdesbieta.com/wp-content/uploads/2018/10/lasagna-original..jpg');
+        $food3->setIngredients(['Lasagna sheets', 'Bechamel sauce', 'Mince meat', 'Tomato sauce', 'Cheese']);
+        $food3->setType('Main');
+        $food3->setOrigin('Italy');
+        $food3->setTime('60');
+
+        $doctrine->persist($food1);
+        $doctrine->persist($food2);
+        $doctrine->persist($food3);
+
+        $doctrine->flush();
+
+        return new Response("Food inserted correctly");
+    }
+
+    #[Route('/food/{id}', name: 'get_recipe')]
+    public function get_recipe(EntityManagerInterface $doctrine, $id): Response
+    {
+        $repository = $doctrine->getRepository(Food::class);
+        $recipe = $repository->find($id);
+        return $this->render('food/showRecipe.html.twig', ["recipe" => $recipe]);
+    }
+}
---
+++ src/Entity/Food.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FoodRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FoodRepository::class)]
+class Food
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $ingredients = [];
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $origin = null;
+
+    #[ORM\Column]
+    private ?int $time = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $difficulty = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIngredients(): array
+    {
+        return $this->ingredients;
+    }
+
+    public function setIngredients(array $ingredients): self
+    {
+        $this->ingredients = $ingredients;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getOrigin(): ?string
+    {
+        return $this->origin;
+    }
+
+    public function setOrigin(?string $origin): self
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+
+    public function getTime(): ?int
+    {
+        return $this->time;
+    }
+
+    public function setTime(int $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?string
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(?string $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
